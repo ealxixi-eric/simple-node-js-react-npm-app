3eed9c11acaaeb5b48f662deb125b5d5f38d5284
xiaoruli <xiaoruli@cisco.com>
2022-06-13 09:56:10 +0800

    ccc

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
             <h2> welcome to cisco </h2>
+             <h3> welcome to ericsson </h3>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
